6b6dce010071752e1a1227c0f9b6c1e6718896ad
avi2verificient <avinash@verificient.com>
2015-11-04 00:52:28 +0530

    hot key deyection update

--- main.cpp
+++ main.cpp
@@ -21,6 +21,11 @@ int main(int argc, char *argv[])
    // RegisterHotKey(NULL,2,MOD_CONTROL | MOD_NOREPEAT,0x44); // print scn
     RegisterHotKey(NULL,5,NULL,VK_SNAPSHOT);  // print scn
     RegisterHotKey(NULL,6,NULL,VK_PRINT); // print
+    RegisterHotKey(NULL,7,MOD_ALT | MOD_NOREPEAT,VK_SNAPSHOT); // alt + print scn
+    //RegisterHotKey(NULL,6,NULL,VK_LEFT); // print
+    RegisterHotKey(NULL,8,NULL,VK_RIGHT); // print
+
+
 
     QApplication a(argc, argv);
    // MainWindow w;
@@ -36,14 +41,70 @@ int main(int argc, char *argv[])
         DispatchMessage(&msg);
         if (msg.message == WM_HOTKEY){
 
-            if (msg.wParam == 1) qDebug() << "Hot Key activated : CTRL + C";
-            if (msg.wParam == 2) qDebug() << "Hot Key activated : CTRL + X";
-            if (msg.wParam == 3) qDebug() << "Hot Key activated : CTRL + V";
-            if (msg.wParam == 4) qDebug() << "Hot Key activated : CTRL + P";
-            if (msg.wParam == 5) qDebug() << "Hot Key activated : Print SCN";
-            if (msg.wParam == 6) qDebug() << "Hot Key activated : PRINT";
-
-
+            if (msg.wParam == 1)
+            {
+                qDebug() << "Hot Key activated : CTRL + C";
+                MessageBox(NULL,(LPCWSTR)L"CTRL + C is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 2)
+            {
+                qDebug() << "Hot Key activated : CTRL + X";
+                MessageBox(NULL,(LPCWSTR)L"CTRL + X is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 3)
+            {
+                qDebug() << "Hot Key activated : CTRL + V";
+                MessageBox(NULL,(LPCWSTR)L"CTRL + V is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 4)
+            {
+                qDebug() << "Hot Key activated : CTRL + P";
+                MessageBox(NULL,(LPCWSTR)L"CTRL + P is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 5)
+            {
+                qDebug() << "Hot Key activated : Print SCN";
+                MessageBox(NULL,(LPCWSTR)L"Print SCN is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 6)
+            {
+                qDebug() << "Hot Key activated : PRINT";
+                MessageBox(NULL,(LPCWSTR)L"Print page is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 7)
+            {
+                qDebug() << "Hot Key activated : PRINT Screen";
+                MessageBox(NULL,(LPCWSTR)L"ALT + Print Scn is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
+            if (msg.wParam == 8)
+            {
+                qDebug() << "Hot Key activated : Right click";
+                MessageBox(NULL,(LPCWSTR)L"Right Click is not allowed?",
+                                (LPCWSTR)L"Restricted Action",
+                                MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+                );
+            }
 
         }
     }
